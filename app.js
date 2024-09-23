82e78c150eebd3787a5f02f979da60a4e8d48e83
Syed Huzaifa <imsyed1998@gmail.com>
2024-09-23 09:51:51 +0500

    commit-2 : async code

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const { readFile,writeFile } = require('fs')
+const { readFile, writeFile } = require('fs')
 
-readFile('./content/first.txt','utf-8',(error,result) => {
-if(error){
-    console.log(error)
-}
-console.log(result)
+readFile('./content/first.txt', 'utf-8', (error, result) => {
+    if (error) {
+        console.log(error)
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf-8', (error, result) => {
+        if (error) {
+            console.log(error)
+        }
+        const second = result;
+        writeFile('./content/result-async.txt', `Here is content of first file: ${first} , 
+            content of second file: ${second}`, (error, result) => {
+            console.log(error)
+        })
+        console.log(result)
+    })
 })
