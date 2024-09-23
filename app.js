0ca1f7853375a21c657e1ed3a19db939fe625959
Syed Huzaifa <imsyed1998@gmail.com>
2024-09-23 17:24:47 +0500

    commit-4 : nodeman package

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 const _ = require('lodash')
 
-const items = [1,2,[3,4,[5,6]]]
+const items = [1,2,[3,4,[5,6,7,8,9.10,10]],[34,45]]
 const itemsNew = _.flattenDeep(items) // this flatten method create linear array convert
 console.log(itemsNew)
+console.log("hel")
